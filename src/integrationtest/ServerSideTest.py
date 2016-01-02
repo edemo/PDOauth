439e07b0c2e8f50cff15a84ec4e64933c40e89e6
Mag <mag@magwas.rulez.org>
2016-01-02 13:15:30 +0100

    500 error fixed. fixes: #203

--- src/integrationtest/ServerSideTest.py
+++ src/integrationtest/ServerSideTest.py
@@ -47,6 +47,35 @@ class ServerSideTest(IntegrationTest, UserTesting, AuthProviderUtil):
             self.assertEqual(responseData['errors'], message)
             self.assertEqual(resp.status_code, status)
 
+    @test
+    def for_access_token_you_need_a_refresh_token(self):
+        with app.test_client() as client:
+            self.app = self.createApp()
+            appid = self.app.appid
+            appsecret = self.app.secret
+            tokenParams = dict(grant_type="refresh_token", client_id=appid, client_secret=appsecret)
+            resp = client.post("/v1/oauth2/token", data=tokenParams)
+        self.assertEqual(400, resp.status_code)
+
+    @test
+    def access_token_can_be_obtained_with_refresh_token(self):
+        with app.test_client() as client:
+            #self.app = self.createApp()
+            appid = self.app.appid
+            appsecret = self.app.secret
+            resp = self.getCode()
+            code = resp.headers['Location'].split('=')[1]
+            paramupdates= dict()
+            resp = self.callTokenInterface(paramupdates, code=code)
+            responseData = self.fromJson(resp)
+            self.assertTrue(responseData.has_key('refresh_token'))
+            refresh_token=responseData['refresh_token']
+            tokenParams = dict(grant_type="refresh_token", client_id=appid, client_secret=appsecret, refresh_token=refresh_token)
+            resp = client.post("/v1/oauth2/token", data=tokenParams)
+        responseData = self.fromJson(resp)
+        self.assertTrue(responseData.has_key('access_token'))
+        self.assertEqual(resp.status_code, 200)
+
     def getCode(self):
         with app.test_client() as client:
             self.login(client)
