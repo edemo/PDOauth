62e1468dccd5cb0ccc9372b7c624deeeeb025a16
mag <mag@magwas.rulez.org>
2016-03-05 09:35:38 +0100

    static

--- static/js/account.js
+++ static/js/account.js
@@ -9,13 +9,10 @@
 		console.log("section:"+section)
 			switch (section) {
 				case "all" :
-					[].forEach.call(document.getElementsByClassName("func"), function (el) {
-						console.log(el);
-						el.style.display="block";
-						});
+				[].forEach.call( document.getElementsByClassName("func"), function (e) { e.style.display="block"; } );
 					break;
 				case "registration" :
-					document.getElementById("registration_section").style.display="block"
+					document.getElementById("register_section").style.display="block"
 					break;
 				case "account" :
 					document.getElementById("my_account_section").style.display="block"
@@ -30,13 +27,21 @@
 					document.getElementById("email_verification_section").style.display="block"
 					break;
 				case "login" :
-				default:
 					document.getElementById("login_section").style.display="block"
+					break;
+				default:
 			}
 		
 		if (self.QueryString.secret) {
 			document.getElementById("PasswordResetForm_secret_input").value=self.QueryString.secret
 		}
+	}
+	PageScript.prototype.hideAllSection=function(){
+		[].forEach.call( document.getElementsByClassName("func"), function (e) { e.style.display="none"; } );
+	}
+	
+	PageScript.prototype.unhideSection=function(section) {
+		document.getElementById(section).style.display="block";
 	}
 
 	PageScript.prototype.displayMsg = function( msg ) {
@@ -45,6 +50,10 @@
 		if (!msg.callback) msg.callback="";
 		this.msgCallback=msg.callback; //only for testing
 		document.getElementById("PopupWindow_CloseButton").onclick = function() {self.closePopup(msg.callback)}
+		document.getElementById("PopupWindow_TitleDiv").innerHTML = msg.title;
+		document.getElementById("PopupWindow_ErrorDiv").innerHTML   = "";
+		document.getElementById("PopupWindow_MessageDiv").innerHTML = "";
+		document.getElementById("PopupWindow_SuccessDiv").innerHTML = ""		
 		if (msg.title) document.getElementById("PopupWindow_TitleDiv").innerHTML = msg.title;
 		if (msg.error) document.getElementById("PopupWindow_ErrorDiv").innerHTML     = "<p class='warning'>"+msg.error+"</p>";
 		if (msg.message) document.getElementById("PopupWindow_MessageDiv").innerHTML = "<p class='message'>"+msg.message+"</p>";
@@ -65,8 +74,8 @@
 		var data = JSON.parse(text);
 		if (status==200) {
 			self.QueryString.uris = data
-			self.uribase = self.QueryString.uris.BACKEND_PATH;
-			keygenForm = document.getElementById("keygenform");
+			self.uribase = self.QueryString.uris.BACKEND_PATH
+			var keygenform = document.getElementById("registration-keygenform")
 			keygenform.action=self.QueryString.uris.BACKEND_PATH+"/v1/keygen"
 			loc = '' + win.location
 			if (loc.indexOf(self.QueryString.uris.SSL_LOGIN_BASE_URL) === 0) {
@@ -75,6 +84,30 @@
 			self.ajaxget("/v1/users/me", self.initCallback)
 		}
 		else self.displayMsg(self.processErrors(data));
-	}	
+	}
+	
+	PageScript.prototype.displayTheSection=function(section) {
+		self.hideAllSection();
+		var lis=document.getElementsByClassName("navbar-nav")[0].getElementsByTagName("li");
+		[].forEach.call( lis, function (e) { e.className=""; } );
+		if (!section){
+			if (self.isLoggedIn){
+
+				self.unhideSection("my_account_section")
+				document.getElementById("nav-bar-my_account").className="active"
+				if (self.isAssurer) self.unhideSection("assurer_section")
+			}
+			else {
+				self.unhideSection("login_section")
+				document.getElementById("nav-bar-login").className="active"
+			}
+		}
+		else {
+			self.unhideSection(section+"_section")
+			var navbar=document.getElementById("nav-bar-"+section)
+			if (navbar) navbar.className="active";
+		}
+	}
+	
 }()
 )
