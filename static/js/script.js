1f6e6e4cadd30e61cf65ff91f98d6f0e7dd72002
root <root@sso.edemokraciagep.org>
2016-03-23 11:56:53 +0100

    add password strength meter and 2nd password field to both registration form #607

--- static/js/script.js
+++ static/js/script.js
@@ -858,7 +858,10 @@ console.log("logoutCallback")
 			switch (self.registrationMethode) {
 				case "pw":
 					console.log('pw')
-					self.register("password")
+					var pwInput=document.getElementById(formName+"registration-form_secret_input")
+					var pwBackup=document.getElementById(formName+"registration-form_secret_backup")
+					if (pwInput.value!=pwBackup.value) self.displayMsg({title:_("Error message"),error:_("The passwords are not identical")})
+					else self.register("password")
 					break;
 				case "fb":
 					console.log('fb')
@@ -910,10 +913,44 @@ console.log("logoutCallback")
 			b.onclick=onclickFunc
 		}
 	}
+
+	PageScript.prototype.passwordChanged = function(formName) {
+		var strength = document.getElementById(formName+"_pw-strength-meter");
+		var strongRegex = new RegExp("^(?=.{10,})((?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[^a-zA-Z0-9_])).*$", "g");
+		var mediumRegex = new RegExp("^(?=.{8,})((?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])).*$", "g");
+		var enoughRegex = new RegExp("(?=.{8,}).*", "g");
+		var pwd = document.getElementById(formName+"_secret_input");
+		if (pwd.value.length==0) {
+			strength.innerHTML = _('Type Password');
+		} 
+		else if (false == enoughRegex.test(pwd.value)) {
+				strength.innerHTML = _("More Characters");
+			} 
+			else if (strongRegex.test(pwd.value)) {
+					strength.innerHTML = '<span style="color:green">'+_("Strong!")+'</span>';
+				} 
+				else if (mediumRegex.test(pwd.value)) {
+						strength.innerHTML = '<span style="color:orange">'+_("Medium!")+'</span>';
+					} 
+					else {
+						strength.innerHTML = '<span style="color:red">'+_("Weak!")+'</span>';	
+					}
+		self.pwEqual(formName)
+	}
+	
+	PageScript.prototype.pwEqual = function(formName) {
+		var pwInput=document.getElementById(formName+"_secret_input")
+		var pwBackup=document.getElementById(formName+"_secret_backup")
+		var pwEqual=document.getElementById(formName+"_pw-equal")
+		if (pwInput.value==pwBackup.value) pwEqual.innerHTML = '<span style="color:green">'+_("OK.")+'</span>';	
+		else pwEqual.innerHTML = '<span style="color:red">'+_("Passwords are not equal.")+'</span>';	
+	}
 }
+	
 pageScript = new PageScript();
 
-/* ==============================================
+/* 
+==============================================
 Back To Top Button
 =============================================== */  
  
