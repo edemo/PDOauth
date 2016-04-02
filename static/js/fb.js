1053ccd3065b63ea3a3f98b5117a15c2e0d22078
root <root@sso.edemokraciagep.org>
2016-04-02 12:08:32 +0200

    fb init according appid coming from uris #676

--- static/js/fb.js
+++ static/js/fb.js
@@ -1,22 +1,3 @@
-  window.fbAsyncInit = function() {
-	  FB.init({
-	    appId      : '517253418423328',
-	    cookie     : true,  // enable cookies to allow the server to access 
-	                        // the session
-	    xfbml      : true,  // parse social plugins on this page
-	    version    : 'v2.2' // use version 2.2
-	  });
-  };
-
-  // Load the SDK asynchronously
-  (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
-
 function FaceBook(pageScript) {
 	this.pageScript = pageScript;
 	this.doc = document;
@@ -50,6 +31,31 @@ function FaceBook(pageScript) {
     });
   }
 
+  	FaceBook.prototype.fbinit = function() {
+		var appID
+		if (appID=this.pageScript.QueryString.uris.FACEBOOK_APP_ID) {
+			window.fbAsyncInit = function() {
+				console.log("fb async init")
+				FB.init({
+					appId      : appID,
+					cookie     : true,  // enable cookies to allow the server to access 
+								// the session
+					xfbml      : true,  // parse social plugins on this page
+					version    : 'v2.2' // use version 2.2
+				});
+			};
+
+			console.log("fbjs loaded");
+  // Load the SDK asynchronously
+			(function(d, s, id) {
+				var js, fjs = d.getElementsByTagName(s)[0];
+				if (d.getElementById(id)) return;
+				js = d.createElement(s); js.id = id;
+				js.src = "//connect.facebook.net/en_US/sdk.js";
+				fjs.parentNode.insertBefore(js, fjs);
+			}(document, 'script', 'facebook-jssdk'));
+		}
+	}
   
 	FaceBook.prototype.credentialCallBack = function(response) {
   		var self = this;
