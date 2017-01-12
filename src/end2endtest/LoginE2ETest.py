8373c73a83623e18aeafeb14666e85a6ee8ec71c
Gerviba <gerviba@gerviba.hu>
2017-01-12 21:42:45 +0000

    Fix LoginE2E typo

--- src/end2endtest/LoginE2ETest.py
+++ src/end2endtest/LoginE2ETest.py
@@ -4,14 +4,8 @@ from unittest.case import TestCase
 
 class LoginE2ETest(TestCase,BrowsingUtil):
 
-    def test_password_can_be_reset_using_the_reset_link(self):
-        password = "Ez3gyJelsz0"
+    def test_login_using_enter(self):
         user = self.getAssurerUser()
-        self.doPasswordResetWithNewPassword(password)
-        oldPassword=user.password
-        user.password=password
-        self.loginWithPasswordAs(user)
+        self.loginWithPasswordAndSubmitAs(user)
         self.assertElementMatchesRe("1","Adataim")
         self.logOut()
-        TE.assurerUser.password=oldPassword
-        self.doPasswordResetWithNewPassword(oldPassword)
--- src/end2endtest/PasswordResetE2ETest.py
+++ src/end2endtest/PasswordResetE2ETest.py
@@ -5,7 +5,13 @@ from unittest.case import TestCase
 class PasswordResetE2ETest(TestCase,BrowsingUtil):
 
     def test_password_can_be_reset_using_the_reset_link(self):
+        password = "Ez3gyJelsz0"
         user = self.getAssurerUser()
-        self.loginWithPasswordAndSubmitAs(user)
+        self.doPasswordResetWithNewPassword(password)
+        oldPassword=user.password
+        user.password=password
+        self.loginWithPasswordAs(user)
         self.assertElementMatchesRe("1","Adataim")
         self.logOut()
+        TE.assurerUser.password=oldPassword
+        self.doPasswordResetWithNewPassword(oldPassword)
