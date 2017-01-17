0a8cb1250dbb0ef3c50c62ff4f5b1ac0296b5980
Mag <mag@magwas.rulez.org>
2017-01-17 17:02:52 +0100

    midair

--- site/test/end2endTests/login.js
+++ site/test/end2endTests/login.js
@@ -1,34 +1,41 @@
+	var PageHeaderTests = function(assert, testFrame) {
+		var self=this;
+		var done=assert.async();
+
+		self.getPageName = function() {
+			t=testFrame.contentWindow
+			uri = t.document.URL.split("/")
+			lastTag = uri[uri.length -1]
+			return lastTag			
+		};
+
+		self.assertPageLoaded = function(pageName) {
+			return function() {
+				assert.equal(self.getPageName(), pageName)
+				done();
+			}
+	    };
+
+	    self.testClickingHomeGoesToIndexHtml = function() {
+			testFrame.onload=self.assertPageLoaded("index.html")
+			var t=testFrame.contentWindow;
+			t.document.getElementById("nav-bar-home").click()
+		};
+		
+	    self.testClickingHowtoGoesToHowto = function() {
+			testFrame.onload=self.assertPageLoaded("user_howto.html")
+			var t=testFrame.contentWindow;
+			t.document.getElementById("nav-bar-howto").click()
+		};
+		return self;
+	};
 
 (function(){
 	var testFrame=document.getElementById('testarea')
 	var t
 	if ( (testFrame) && (t=testFrame.contentWindow) ) {}
 	else return
-
-	var PageHeaderTests = function(assert, done, testFrame) {
-		var self=this;
-		
-		self.getLastTag = function() {
-			console.log("getLastTag")
-			t=testFrame.contentWindow
-			uri = t.document.URL.split("/")
-			lastTag = uri[uri.length -1]
-			return lastTag			
-		};
-		self.assertIndexHtmlLoaded = function() {
-			console.log("assertIndexHtmlLoaded")
-			assert.equal(self.getLastTag(), "index.html")
-			done();
-	    };
-	    self.testClickingHomeGoesToIndexHtml = function() {
-			console.log("testClickingHomeGoesToIndexHtml")
-			testFrame.onload=self.assertIndexHtmlLoaded
-			var t=testFrame.contentWindow;
-			t.document.getElementById("nav-bar-home").click()
-		};
-		return self;
-	}
-
+	
 	QUnit.module( "login" );
 	QUnit.test( "loading fiokom.html", function( assert ) {
 		var done=assert.async()
@@ -48,16 +55,44 @@
 		tfrwrk.loadPage("index.html",callback);
 	});
 	
-	QUnit.test( "clicking on home loads index.html", function( assert ) {		
-		var done=assert.async();
-		var callback=PageHeaderTests(assert, done, testFrame).testClickingHomeGoesToIndexHtml
+	QUnit.test( "clicking on home loads index.html (account)", function( assert ) {
+		var callback=PageHeaderTests(assert, testFrame).testClickingHomeGoesToIndexHtml
+		tfrwrk.loadPage("fiokom.html",callback);
+	});
+
+	QUnit.test( "clicking on home loads index.html (index)", function( assert ) {		
+		var callback=PageHeaderTests(assert, testFrame).testClickingHomeGoesToIndexHtml
 		tfrwrk.loadPage("fiokom.html",callback);
 	});
 
 	QUnit.test( "clicking on home loads index.html (about us)", function( assert ) {		
-		var done=assert.async();
-		var callback=PageHeaderTests(assert, done, testFrame).testClickingHomeGoesToIndexHtml
+		var callback=PageHeaderTests(assert, testFrame).testClickingHomeGoesToIndexHtml
 		tfrwrk.loadPage("about_us.html",callback);
+	});
+
+	QUnit.test( "clicking on home loads index.html (user_howto)", function( assert ) {		
+		var callback=PageHeaderTests(assert, testFrame).testClickingHomeGoesToIndexHtml
+		tfrwrk.loadPage("user_howto.html",callback);
+	});
+
+	QUnit.test( "clicking on howto loads user_howto.html (account)", function( assert ) {
+		var callback=PageHeaderTests(assert, testFrame).testClickingHowtoGoesToHowto
+		tfrwrk.loadPage("fiokom.html",callback);
+	});
+
+	QUnit.test( "clicking on howto loads user_howto.html (index)", function( assert ) {
+		var callback=PageHeaderTests(assert, testFrame).testClickingHowtoGoesToHowto
+		tfrwrk.loadPage("index.html",callback);
+	});
+
+	QUnit.test( "clicking on howto loads user_howto.html (about us)", function( assert ) {
+		var callback=PageHeaderTests(assert, testFrame).testClickingHowtoGoesToHowto
+		tfrwrk.loadPage("about_us.html",callback);
+	});
+
+	QUnit.test( "clicking on howto loads user_howto.html (user howto)", function( assert ) {
+		var callback=PageHeaderTests(assert, testFrame).testClickingHowtoGoesToHowto
+		tfrwrk.loadPage("user_howto.html",callback);
 	});
 
 }())
