d847af89748989e957a48442c960fde503503168
György Peng <gypeng@drezina.hu>
2017-02-01 10:40:25 +0100

    Create logout.js

---
+++ site/js/logout.js
@@ -0,0 +1,18 @@
+import Ajax from './modules/ajax.js'
+var ajax = new Ajax(), c
+
+
+getCookie = function(cname) {
+    var name = cname + "=",
+        ca = window.document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+	    while (c.charAt(0)==' ') c = c.substring(1);
+	    if (c.indexOf(name) == 0) {
+		    return c.substring(name.length,c.length);
+		}
+    }
+    return false;
+} 
+
+if ( !(c=getCookie(sso_no_app_logout)) || c!="true" ) ajax.get( "/v1/logout" )
