781030c7fdd9113a2ff086242b76b26d1ae8bedf
Claymanus <gypeng@drezina.hu>
2017-02-01 19:33:46 +0100

    autologout fixes

--- site/js/logout.js
+++ site/js/logout.js
@@ -1,20 +1,23 @@
 import Ajax from './modules/ajax.js'
-var ajax = new Ajax(), c
-
-
-getCookie = function(cname) {
-    var name = cname + "=",
-        ca = window.document.cookie.split(';');
-    for(var i=0; i<ca.length; i++) {
-        var c = ca[i];
-	    while (c.charAt(0)==' ') c = c.substring(1);
-	    if (c.indexOf(name) == 0) {
-		    return c.substring(name.length,c.length);
+var ajax = new Ajax({}),
+	c,
+	getCookie = function(cname) {
+		var name = cname + "=",
+			ca = window.document.cookie.split(';');
+		for(var i=0; i<ca.length; i++) {
+			var c = ca[i];
+			while (c.charAt(0)==' ') c = c.substring(1);
+			if (c.indexOf(name) == 0) {
+				return c.substring(name.length,c.length);
+			}
 		}
-    }
-    return false;
-} 
-
-var next = function(){document.getElementById("flag").style.display="block"}
-
-if ( !(c=getCookie("sso_no_app_logout")) || c!="true" ) ajax.get( "/v1/logout", {next:next, error:next} )
+		return false;
+	},
+	tracker = document.getElementById("flag"),
+	next = function(){ tracker.style.display = "block"	},
+	logout = function( result ) {
+		ajax.uribase = JSON.parse(result).BACKEND_PATH;
+		ajax.get( "/v1/logout", {next:next, error:next} )
+	};
+if ( !(c=getCookie("sso_no_app_logout")) || c=="true" ) ajax.get("/adauris", {next:logout, error:next}, true)
+else next();
