de4e4288f48b4bac03e3c8ca05a9c81fb2a215f6
Laszlo Barjak <barjak.laszlo@gmail.com>
2017-03-06 16:50:23 +0100

    Update jsdom-test.js

--- site/js/unittests/jsdom-test.js
+++ site/js/unittests/jsdom-test.js
@@ -3,4 +3,4 @@ var document = jsdom("<html><body></body></html>");
 var p = document.createElement("p");
 document.body.appendChild(p);
 document.getElementsByTagName("p").innerHTML = "******** jsdom DOM test ready ********";
-console.log(document.getElementsByTagName("p").innerHTML);
+console.log(document.getElementsByTagName("p").innerHTML)
