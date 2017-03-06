cb6734fe9d8685f919ed497a1efccd9a2068e07f
Laszlo Barjak <barjak.laszlo@gmail.com>
2017-03-06 16:46:09 +0100

    jsdom test

---
+++ site/js/unittests/jsdom-test.js
@@ -0,0 +1,6 @@
+var jsdom = require("jsdom").jsdom;
+var document = jsdom("<html><body></body></html>");
+var p = document.createElement("p");
+document.body.appendChild(p);
+document.getElementsByTagName("p").innerHTML = "******** jsdom DOM test ready ********";
+console.log(document.getElementsByTagName("p").innerHTML);
