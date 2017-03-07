4da03eea4667dee5906c82244d1d031e0abaa430
Laszlo Barjak <barjak.laszlo@gmail.com>
2017-03-07 10:27:56 +0100

    Update jsdom-test.js

--- site/js/unittests/jsdom-test.js
+++ site/js/unittests/jsdom-test.js
@@ -1,6 +1,14 @@
+"use strict";
+
 var jsdom = require("jsdom").jsdom;
 var document = jsdom("<html><body></body></html>");
 var p = document.createElement("p");
 document.body.appendChild(p);
-document.getElementsByTagName("p").innerHTML = "******** jsdom DOM test ready ********";
-console.log(document.getElementsByTagName("p").innerHTML)
+var str = "******** jsdom DOM test ********"
+document.getElementsByTagName("p").innerHTML = str;
+var res = document.getElementsByTagName("p").innerHTML;
+
+QUnit.module( "test" ); 
+QUnit.test( "jsdom DOM test", function( assert ) {
+	assert.equal(str, res, "na" );
+})
