468162af7a2799c01d1c06d79f942d3357ec0981
Laszlo Barjak <barjak.laszlo@gmail.com>
2017-03-11 14:27:27 +0100

    Update test.js

--- site/js/unittests/test.js
+++ site/js/unittests/test.js
@@ -1,7 +1,6 @@
 var QUnit = require ('qunit-cli');
 QUnit.load();
 
-console.log("hello world")
 QUnit.module( "test" ); 
 QUnit.test( "basic environment test", function( assert ) {
         assert.equal("1","1");
